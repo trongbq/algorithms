8912a23f35d8d4bc615505b81cca5c479eeb7817
TrongBQ <trong.buiquoc@gmail.com>
2020-04-30 21:16:33 +0700

    Add find shortest-path dijkstra

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -17,10 +17,9 @@ struct vertex {
 };
 
 class Graph {
-    int size;
     vector<vector<int> > adj; // adjceacy-matrix with weight
 private:
-    int findMinDistanceVertex(map<int, int> distances, vector<bool> visited);
+    int findVertexWithMinDistance(map<int, int> distances, vector<bool> visited);
     vector<int> findNeighbors(int v);
 public:
     Graph(int v);
@@ -30,7 +29,6 @@ public:
 };
 
 Graph::Graph(int v) {
-    size = v;
     for (int i = 0; i < v; i++) {
         vector<int> vec;
         for (int j = 0; j < v; j++) {
@@ -45,44 +43,65 @@ void Graph::addEdge(int u, int v, int w) {
 }
 
 void Graph::dijkstra(int s) {
-    map<int, int> distances;
-    map<int, int> parents;
-    vector<bool> visited;
+    map<int, int> distances; // Store current shortest-path distance from parent to a vertices
+    map<int, int> parents; // Store parents of a vertices
+    vector<bool> visited; // Mark vertices that are visited
     
+    // Initialize data
     for (int i = 0; i < adj.size(); i++) {
         distances[i] = INFINITY;
-        visited[i] = false;
+        visited.push_back(false);
     }
     distances[s] = 0;
-    visited[s] = true;
     
-    int v = findMinDistanceVertex(distances, visited);
-    while (next != INVALID_VERTEX) {
+    // Start the calculation of shortest-path for all vertices
+    int v = findVertexWithMinDistance(distances, visited);
+    while (v != INVALID_VERTEX) {
         vector<int> neighbors = findNeighbors(v);
-        for (vector<int>::iterator it = neighbors.begin(); it != neighbors.end(); i++) {
-            int nd = distances[next] + adj[next][*it];
+
+        for (vector<int>::iterator it = neighbors.begin(); it != neighbors.end(); it++) {
+            int nd = distances[v] + adj[v][*it];
             if (distances[*it] > nd) {
                 distances[*it] = nd;
                 parents[*it] = v;
             }
         }
         visited[v] = true;
+        v = findVertexWithMinDistance(distances, visited);
+    }
+    
+    // Display the results
+    cout << "Dijkstra:" << endl;
+    cout << "Parent\tVertex\tDistance" << endl;
+    for (int i = 0; i < adj.size(); i++) {
+        cout << parents[i] << "\t" << i << "\t" << distances[i] << endl;
     }
 }
 
-int Graph::findMinDistanceVertex(map<int, int> distances, vector<bool> visited) {
-    return 0;
+// Find the vertex that is not visited and has smallest shortest-path distance
+int Graph::findVertexWithMinDistance(map<int, int> distances, vector<bool> visited) {
+    int min = INFINITY;
+    int v = INVALID_VERTEX;
+
+    for (map<int, int>::iterator it = distances.begin(); it != distances.end(); it++) {
+        if (!visited[it->first] && min > it->second) {
+            min = it->second;
+            v = it-> first;
+        }
+    }
+
+    return v;
 }
 
 vector<int> Graph::findNeighbors(int v) {
-    vector<int> v;
-    for (int i = 0; i < size; i++) {
+    vector<int> neighbors;
+    for (int i = 0; i < adj.size(); i++) {
         if (adj[v][i] != NO_PATH) {
-            v.push_back(i);
+            neighbors.push_back(i);
         }
     }
     
-    return v;
+    return neighbors;
 }
 
 void Graph::display() {
@@ -90,14 +109,18 @@ void Graph::display() {
     
     cout << "   ";
     for (int i = 0; i < adj.size(); i++) {
-        cout << i << "   ";
+        cout << i << "\t";
     }
     cout << endl;
     
     for (int i = 0; i < adj.size(); i++) {
         cout << i << "  ";
         for (vector<int>::iterator it = adj[i].begin(); it != adj[i].end(); it++) {
-            cout << *it << "   ";
+            if (*it == NO_PATH) {
+                cout << "-\t";
+            } else {
+                cout << *it << "\t";
+            }
         }
         cout << endl;
     }
@@ -136,6 +159,7 @@ int main()
   g.addEdge(8, 7, 7);
   
   g.display();
+  g.dijkstra(0);
   
   return 0;
 }
