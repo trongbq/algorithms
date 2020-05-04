d6fb2fdf8d17c9380f0f2d8653f56c66f4fda0c2
TrongBQ <trong.buiquoc@gmail.com>
2020-05-04 23:07:16 +0700

    Add basic trie struct

---
+++ data_structure/trie.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const unsigned int ALPHABET_SIZE = 26;
+
+class TreeNode {
+    vector<TreeNode> children;
+    bool isEnd;
+};
+
+int main()
+{
+    TreeNode a = TreeNode();
+    
+    return 0;
+}
